c18bf7edd256c6980c5356d5d0444da9a42cee6d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-02-27 17:45:55 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/browser/MainActivity.java
+++ app/src/main/java/com/coara/browser/MainActivity.java
@@ -504,8 +504,6 @@ private WebView createNewWebView() {
     webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
 
     WebSettings settings = webView.getSettings();
-    String defaultUA = settings.getUserAgentString();
-    originalUserAgents.put(webView, defaultUA);
     applyOptimizedSettings(settings);
     
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
