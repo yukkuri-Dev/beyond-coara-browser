4b0f9b8c26bb5d4121c82cf7a14c1c16c34d85c4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-02-28 08:11:10 +0900

    Update asciiart.java

--- app/src/main/java/com/coara/browser/asciiart.java
+++ app/src/main/java/com/coara/browser/asciiart.java
@@ -2,6 +2,7 @@ package com.coara.browser;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
