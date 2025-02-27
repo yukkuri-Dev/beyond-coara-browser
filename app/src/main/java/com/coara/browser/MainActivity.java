b9f09e4a91451959bcb63abf3d7f326c2119752c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-02-27 17:30:57 +0900

    テスト83

--- app/src/main/java/com/coara/browser/MainActivity.java
+++ app/src/main/java/com/coara/browser/MainActivity.java
@@ -80,6 +80,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
