997212a5155539f387bb85ee256236efc4243433
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-03-28 14:33:04 +0900

    Add files via upload

---
+++ app/src/main/java/com/coara/browser/SettingItem.java
@@ -0,0 +1,25 @@
+package com.coara.browser;
+
+public class SettingItem {
+    private String title;
+    private String description;
+    private Runnable action;
+
+    public SettingItem(String title, String description, Runnable action) {
+        this.title = title;
+        this.description = description;
+        this.action = action;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Runnable getAction() {
+        return action;
+    }
+}
