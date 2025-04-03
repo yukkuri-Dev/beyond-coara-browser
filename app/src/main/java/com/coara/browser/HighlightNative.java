2d7b83f895a850aca1abf00fb8e99c4ddbd8597a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-04-03 23:10:07 +0900

    Delete app/src/main/java/com/coara/browser/HighlightNative.java

--- app/src/main/java/com/coara/browser/HighlightNative.java
+++
@@ -1,9 +0,0 @@
-package com.coara.browser;
-
-public class HighlightNative {
-    static {
-        System.loadLibrary("highlight_native");
-    }
-    
-    public static native int[][] highlightHtmlNative(String html);
-}
