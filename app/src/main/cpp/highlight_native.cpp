90b70cee88360140733ce9d1ca6a0010db1011d9
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-04-03 23:42:44 +0900

    Update highlight_native.cpp

--- app/src/main/cpp/highlight_native.cpp
+++ app/src/main/cpp/highlight_native.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <algorithm>
 
 struct HighlightSpan {
     int start;
@@ -12,57 +13,76 @@ struct HighlightSpan {
 extern "C" {
 
 JNIEXPORT jobjectArray JNICALL
-Java_com_coara_browser_htmlview_highlightHtmlNative(JNIEnv* env, jclass clazz, jstring htmlStr) {
-  
-    const char* cstr = env->GetStringUTFChars(htmlStr, nullptr);
-    std::string html(cstr);
-    env->ReleaseStringUTFChars(htmlStr, cstr);
+Java_com_coara_browser_htmlview_diffHighlightNative(JNIEnv* env, jclass clazz,
+                                                      jstring oldTextJ, jstring newTextJ) {
 
-  
-    const int tagColor       = 0xFF0000FF; 
-    const int attributeColor = 0xFF008000; 
-    const int valueColor     = 0xFFB22222; 
+    const char* oldTextC = env->GetStringUTFChars(oldTextJ, nullptr);
+    const char* newTextC = env->GetStringUTFChars(newTextJ, nullptr);
+    std::string oldText(oldTextC), newText(newTextC);
+    env->ReleaseStringUTFChars(oldTextJ, oldTextC);
+    env->ReleaseStringUTFChars(newTextJ, newTextC);
 
-    
+
+    int diffStart = 0;
+    int minLen = std::min(oldText.size(), newText.size());
+    while (diffStart < minLen && oldText[diffStart] == newText[diffStart]) {
+        diffStart++;
+    }
+
+
+    int diffEndOld = oldText.size();
+    int diffEndNew = newText.size();
+    while (diffEndOld > diffStart && diffEndNew > diffStart &&
+           oldText[diffEndOld - 1] == newText[diffEndNew - 1]) {
+        diffEndOld--;
+        diffEndNew--;
+    }
+
+
+    std::string diffSegment = newText.substr(diffStart, diffEndNew - diffStart);
+
+
+    const int tagColor       = 0xFF0000FF;  // 青
+    const int attributeColor = 0xFF008000;  // 緑
+    const int valueColor     = 0xFFB22222;  // 茶
+
     std::regex tag_regex("<[^>]+>");
     std::regex attr_regex("(\\w+)=\\\"([^\\\"]*)\\\"");
-
     std::vector<HighlightSpan> spans;
 
-  
-    auto tag_begin = std::sregex_iterator(html.begin(), html.end(), tag_regex);
+    auto tag_begin = std::sregex_iterator(diffSegment.begin(), diffSegment.end(), tag_regex);
     auto tag_end = std::sregex_iterator();
     for (auto it = tag_begin; it != tag_end; ++it) {
         std::smatch tagMatch = *it;
-        int tagStart = static_cast<int>(tagMatch.position());
+        
+        int tagStart = static_cast<int>(tagMatch.position()) + diffStart;
         int tagEndPos = tagStart + static_cast<int>(tagMatch.length());
         spans.push_back({ tagStart, tagEndPos, tagColor });
 
-      
         std::string tagText = tagMatch.str();
         auto attr_begin = std::sregex_iterator(tagText.begin(), tagText.end(), attr_regex);
         auto attr_end = std::sregex_iterator();
         for (auto ait = attr_begin; ait != attr_end; ++ait) {
             std::smatch attrMatch = *ait;
             int attrNameStart = tagStart + static_cast<int>(attrMatch.position(1));
-            int attrNameEnd   = attrNameStart + static_cast<int>(attrMatch.length(1));
+            int attrNameEnd = attrNameStart + static_cast<int>(attrMatch.length(1));
             spans.push_back({ attrNameStart, attrNameEnd, attributeColor });
             int attrValueStart = tagStart + static_cast<int>(attrMatch.position(2));
-            int attrValueEnd   = attrValueStart + static_cast<int>(attrMatch.length(2));
+            int attrValueEnd = attrValueStart + static_cast<int>(attrMatch.length(2));
             spans.push_back({ attrValueStart, attrValueEnd, valueColor });
         }
     }
 
+    
     jclass intArrayClass = env->FindClass("[I");
-    jobjectArray result = env->NewObjectArray(static_cast<jsize>(spans.size()), intArrayClass, nullptr);
+    jobjectArray jresult = env->NewObjectArray(static_cast<jsize>(spans.size()), intArrayClass, nullptr);
     for (size_t i = 0; i < spans.size(); i++) {
         jint arr[3] = { spans[i].start, spans[i].end, spans[i].color };
         jintArray intArr = env->NewIntArray(3);
         env->SetIntArrayRegion(intArr, 0, 3, arr);
-        env->SetObjectArrayElement(result, static_cast<jsize>(i), intArr);
+        env->SetObjectArrayElement(jresult, static_cast<jsize>(i), intArr);
         env->DeleteLocalRef(intArr);
     }
-    return result;
+    return jresult;
 }
-
-} 
+}
