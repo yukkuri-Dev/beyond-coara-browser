9230f5f185df4ebae9f9d95d21ae68e1c3d38d8a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-04-09 05:49:20 +0900

    Update SecretActivity.java

--- app/src/main/java/com/coara/browser/SecretActivity.java
+++ app/src/main/java/com/coara/browser/SecretActivity.java
@@ -232,25 +232,7 @@ public class SecretActivity extends AppCompatActivity {
     boolean shouldClear = getIntent()
             .getBooleanExtra(MainActivity.EXTRA_CLEAR_HISTORY, false);
         if (shouldClear) {
-            WebView current = getCurrentWebView();
-                current.clearHistory();
-            historyItems.clear();
-            saveHistory();
-            clearWebStorage();
-            clearPageCache();
-            clearTabs();
-            WebViewDatabase.getInstance(SecretActivity.this).clearFormData();
-            CookieManager cookieManager = CookieManager.getInstance();
-            cookieManager.removeAllCookies(null);
-            cookieManager.flush();
-            urlEditText.setInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_URI | EditorInfo.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-            urlEditText.setRawInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_URI | EditorInfo.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-            urlEditText.setPrivateImeOptions("nm");
-            urlEditText.setAutofillHints("");
-            urlEditText.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO);
-            String currentText = urlEditText.getText().toString();
-            urlEditText.setText("");
-            urlEditText.setText(currentText);
+            clear0();
     }
     setContentView(R.layout.secret_main);
 
@@ -413,6 +395,20 @@ public class SecretActivity extends AppCompatActivity {
             }
             return false;
         });
+    private void clear0() {
+        WebView webView = getCurrentWebView();
+    if (webView != null) {
+        webView.clearHistory();
+        webView.clearCache(true);
+        webView.clearFormData();
+        webView.clearSslPreferences();
+        webView.clearMatches();
+      }
+    WebStorage.getInstance().deleteAllData();
+    CookieManager cookieManager = CookieManager.getInstance();
+    cookieManager.removeAllCookies(null);
+    cookieManager.flush();
+     }
 
         btnNewTab.setOnClickListener(v -> createNewTab());
 
